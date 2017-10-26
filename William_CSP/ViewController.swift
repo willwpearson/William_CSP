5148aaac6948fba9d37855b6d40581bf8b7a1d74
willwpearson <31518711+willwpearson@users.noreply.github.com>
2017-10-26 11:46:15 -0600

    Created a new project for CSP 7.

---
+++ William_CSP/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  William_CSP
+//
+//  Created by Pearson, William on 10/26/17.
+//  Copyright © 2017 CTEC. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
