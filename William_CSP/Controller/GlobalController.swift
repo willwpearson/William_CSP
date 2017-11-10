4433a763d1c738d8d5faa76f24a77f5f89e353c8
willwpearson <31518711+willwpearson@users.noreply.github.com>
2017-11-10 13:46:00 -0700

    Wrote the code for the setup method on the global page, and linked in the labels and images needed.

--- William_CSP/Controller/GlobalController.swift
+++ William_CSP/Controller/GlobalController.swift
@@ -8,6 +8,28 @@
 
 import UIKit
 
-class GlobalController: UIViewController {
+class GlobalController: UIViewController
+{
+    @IBOutlet weak var firstImage: UIImageView!
+    @IBOutlet weak var problemLabel: UILabel!
+    @IBOutlet weak var personalLabel: UILabel!
 
+    private func setupGlobal() -> Void
+    {
+        firstImage.image = UIImage(named: ")
+        
+        var problemString : String
+        problemString = "This problem is worldwide, and there are a large number of deaths amounted to this every single day."
+        problemLabel.text = problemString
+        
+        var personalString : String
+        personalString = "This problem is personal to me because there are water problems all over the globe. I have relatives in Oregon which is currently getting into a drought and we are worried for them."
+        personalLabel.text = personalString
+    }
+    
+    override public func viewDidLoad()
+    {
+        super.viewDidLoad()
+        setupGlobal()
+    }
 }
