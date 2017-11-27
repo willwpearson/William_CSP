f2dfc7fd5f0039112cc5f7ad4f93cae661f179a8
willwpearson <31518711+willwpearson@users.noreply.github.com>
2017-11-27 12:34:47 -0700

    Added the second required protocol method, the swipe right one.

--- William_CSP/Controller/AbstractionController.swift
+++ William_CSP/Controller/AbstractionController.swift
@@ -67,4 +67,29 @@ public class AbstractionController: UIPageViewController, UIPageViewControllerDa
         
         return orderedAbstractionViews[previousIndex]
     }
+    
+    public func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController?
+    {
+        guard let viewControllerIndex = orderedAbstractionViews.index(of: viewController)
+        else
+        {
+            return nil
+        }
+        
+        let nextIndex = viewControllerIndex + 1
+        
+        guard nextIndex >= 0
+        else
+        {
+            return nil
+        }
+        
+        guard nextIndex < orderedAbstractionViews.count
+        else
+        {
+            return orderedAbstractionViews.first
+        }
+        
+        return orderedAbstractionViews[nextIndex]
+    }
 }
