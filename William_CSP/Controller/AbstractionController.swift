f8fc237e84c7453cfeb89c43b4078fbf37f936ec
willwpearson <31518711+willwpearson@users.noreply.github.com>
2017-11-27 12:51:38 -0700

    Added the methods for the dots in the bottom of the screen.

--- William_CSP/Controller/AbstractionController.swift
+++ William_CSP/Controller/AbstractionController.swift
@@ -92,4 +92,21 @@ public class AbstractionController: UIPageViewController, UIPageViewControllerDa
         
         return orderedAbstractionViews[nextIndex]
     }
+    
+    //MARK:- Support for the dots in the UIPageViewController
+    public func presentationCount(for pageViewController: UIPageViewController) -> Int
+    {
+        return orderedAbstractionViews.count
+    }
+    
+    public func presentationIndex(for pageViewController: UIPageViewController) -> Int
+    {
+        guard let firstViewController = viewControllers?.first, let firstViewControllerIndex = orderedAbstractionViews.index(of: firstViewController)
+        else
+        {
+            return 0
+        }
+        
+        return firstViewControllerIndex
+    }
 }
