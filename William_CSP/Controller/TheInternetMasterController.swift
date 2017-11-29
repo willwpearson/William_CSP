f4fa26dfd168fd245f804d38cb4b0591f1175e84
willwpearson <31518711+willwpearson@users.noreply.github.com>
2017-11-29 11:51:55 -0700

    Renamed a class to match the Master Controller, and added the tableView methods and internet topics array.

---
+++ William_CSP/Controller/TheInternetMasterController.swift
@@ -0,0 +1,62 @@
+//
+//  TheInternetController.swift
+//  William_CSP
+//
+//  Created by Pearson, William on 10/26/17.
+//  Copyright © 2017 CTEC. All rights reserved.
+//
+
+import UIKit
+
+public class TheInternetMasterController: UITableViewController
+{
+    private (set) lazy var internetTopics : [String] =
+    {
+        return [
+            "Definitions",
+            "CSP",
+            "CTEC",
+            "Canyons",
+            "Twitter",
+            "Swift Guide"
+        ]
+    }()
+    
+    private var detailViewController : InternetDetailViewController?
+    
+    private func setup() -> Void
+    {
+        
+    }
+    
+    override public func viewDidLoad()
+    {
+        super.viewDidLoad()
+        setup()
+        // Uncomment the following line to preserve selection between presentation
+        self.clearsSelectionOnViewWillAppear = false
+    }
+    
+    // MARK: - Table view data source
+    
+    ovverride public func numberOfSections(in tableView: UITableView) -> Int
+    {
+        // #warning Incomplete implementation, return the number of sections
+        return 1
+    }
+    
+    override public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return internetTopics.count
+    }
+    
+    override public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let currentText = internetTopics[indexPath.row]
+        cell.textLabel!.text = currentText
+        
+        return cell
+    }
+    
+}
