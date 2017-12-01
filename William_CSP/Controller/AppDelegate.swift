e4be1360dff019670567a7c22f422734336b6c55
willwpearson <31518711+willwpearson@users.noreply.github.com>
2017-12-01 13:49:53 -0700

    Added the proper if let into the app delegate.

--- William_CSP/Controller/AppDelegate.swift
+++ William_CSP/Controller/AppDelegate.swift
@@ -9,13 +9,24 @@
 import UIKit
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate, UISplitViewControllerDelegate {
 
     var window: UIWindow?
 
 
-    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
+    func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool
+    {
         // Override point for customization after application launch.
+        
+        //MARK:- Master-Detail code
+        if let splitViewController = window!.rootViewController as?UISplitViewController
+        {
+            let navigationController = splitViewController.viewControllers[splitViewController.viewControllers.count - 1] as! UINavigationController
+            
+            navigationController.topViewController!.navigationItem.leftBarButtonItem = splitViewController.displayModeButtonItem
+            splitViewController.delegate = self
+        }
+        
         return true
     }
 
