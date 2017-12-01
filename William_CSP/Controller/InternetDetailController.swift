5a512082ad862370a06a005e25481c38a8ded55b
willwpearson <31518711+willwpearson@users.noreply.github.com>
2017-12-01 13:18:34 -0700

    Added the proper data members and the load method for the Detail View.

--- William_CSP/Controller/InternetDetailController.swift
+++ William_CSP/Controller/InternetDetailController.swift
@@ -15,16 +15,64 @@ public class InternetDetailController : UIViewController
     @IBOutlet weak var textView: UILabel!
     @IBOutlet weak var webViewer: WKWebView!
     
+    var detailAddress : String?
+    {
+        // MARK: Update the detail view when a value is changed.
+        didSet
+        {
+            configureDetailView()
+        }
+    }
+    
+    var detailText : String?
+    {
+        // MARK: Update the detail view when a value is changed.
+        didSet
+        {
+            configureDetailView()
+        }
+    }
     
     override public func viewDidLoad()
     {
         super.viewDidLoad()
-        setup()
+        configureDetailView()
         // Do any additional setup after loading the view.
     }
     
-    private func setup() -> Void
+    private func configureDetailView() -> Void
     {
-    
+        if detailAddress != nil
+        {
+            if let currentWebView = webViewer
+            {
+                let currentURL = URL(string: detailAddress!)
+                let currentWebRequest = URLRequest(url: currentURL!)
+                currentWebView.load(currentWebRequest)
+            }
+        }
+        else
+        {
+            if let currentWebView = webViewer
+            {
+                let currentURL = URL(string: "https://www.cnn.com")
+                currentWebView.load(URLRequest(url:currentURL!))
+            }
+        }
+        
+        if detailText != nil
+        {
+            if let currentText = textView
+            {
+                currentText.text = detailText
+            }
+        }
+        else
+        {
+            if let currentText = textView
+            {
+                currentText.text = "William's CSP Internet Screen."
+            }
+        }
     }
 }
