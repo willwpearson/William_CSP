410a1e4afcc01524f201c236992bf6693887a9a0
willwpearson <31518711+willwpearson@users.noreply.github.com>
2017-12-05 12:37:12 -0700

    Added the draw method to draw a stick figure.

---
+++ William_CSP/View/DrawingView.swift
@@ -0,0 +1,46 @@
+//
+//  DrawingView.swift
+//  William_CSP
+//
+//  Created by Pearson, William on 12/5/17.
+//  Copyright © 2017 CTEC. All rights reserved.
+//
+
+import UIKit
+
+public class DrawingView: UIView
+{
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adverselffects performance during animation.
+    override public func draw(_ rect: CGRect)
+    {
+        // Drawing code
+        drawStickFigure().stroke()
+        //drawTurtle()
+        //drawHappyTree()
+    }
+    
+    private func drawStickFigure() -> UIBezierPath
+    {
+        let stickFigure = UIBezierPath()
+        
+        UIColor.cyan.setStroke()
+        stickFigure.lineWidth = 3.0
+        
+        stickFigure.addArc(withCenter: CGPoint(x: 200, y: 200),
+                           radius: CGFloat(20),
+                           startAngle: CGFloat(0),
+                           endAngle: CGFloat(2) * CGFloat.pi,
+                           clockwise: true)
+        stickFigure.move(to: CGPoint(x: 200, y: 220))
+        stickFigure.addLine(to: CGPoint(x: 200, y: 270))
+        stickFigure.move(to: CGPoint(x:180, y: 240))
+        stickFigure.addLine(to: CGPoint(x: 220, y: 240))
+        stickFigure.move(to: CGPoint(x: 200, y: 270))
+        stickFigure.addLine(to: CGPoint(x: 180, y: 300))
+        stickFigure.move(to: CGPoint(x: 200, y: 270))
+        stickFigure.addLine(to: CGPoint(x: 220, y: 300))
+        
+        return stickFigure
+    }
+}
