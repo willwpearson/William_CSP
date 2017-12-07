ec953d818e9e84c9a076f4fd21b935e4598d8b2f
willwpearson <31518711+willwpearson@users.noreply.github.com>
2017-12-07 12:53:38 -0700

    Added the ArtCell class with the outlets for an image view and an image label.

---
+++ William_CSP/Controller/ArtCell.swift
@@ -0,0 +1,17 @@
+//
+//  ArtCell.swift
+//  William_CSP
+//
+//  Created by Pearson, William on 12/7/17.
+//  Copyright © 2017 CTEC. All rights reserved.
+//
+
+import UIKit
+
+public class ArtCell : UICollectionViewCell
+{
+    
+    @IBOutlet weak var imageView: UIImageView!
+   
+    @IBOutlet weak var imageName: UILabel!
+}
