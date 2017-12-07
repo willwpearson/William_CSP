256d818da0fb5b5d92bb83e917c2faecab9afdaa
willwpearson <31518711+willwpearson@users.noreply.github.com>
2017-12-07 11:54:22 -0700

    Added the data member largePhotoIndexPath.

--- William_CSP/Controller/CreativityController.swift
+++ William_CSP/Controller/CreativityController.swift
@@ -31,5 +31,34 @@ public class CreativityController: UICollectionViewController, UICollectionViewD
             ]
     }()
     
-    
+    var largePhotoIndexPath: IndexPath?
+    {
+        didSet
+        {
+            var indexPaths = [IndexPath]()
+            if let largePhotoIndexPath = largePhotoIndexPath
+            {
+                indexPaths.append(largePhotoIndexPath)
+            }
+            if let oldValue = oldValue
+            {
+                indexPaths.append(oldValue)
+            }
+            
+            collectionView?.performBatchUpdates(
+                {
+                self.collectionView?.reloadItems(at: indexPaths)
+                })
+            {
+                completed in
+                
+                if let largePhotoIndexPath = self.largePhotoIndexPath
+                {
+                    self.collectionView?.scrollToItem(at: largePhotoIndexPath,
+                                                      at: .centeredVertically,
+                                                      animated: true)
+                }
+            }
+        }
+    }
 }
