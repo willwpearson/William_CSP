909d7c1b95128e963042a749f244d477af7706d4
willwpearson <31518711+willwpearson@users.noreply.github.com>
2017-12-13 13:50:20 -0700

    Added the init for the invader laser class, and removed an already written method.

--- William_CSP/Model/InvaderLaser.swift
+++ William_CSP/Model/InvaderLaser.swift
@@ -9,22 +9,23 @@
 import UIKit
 import SpriteKit
 
-public class InvaderBullet: Projectile
+public class InvaderLaser: Projectile
 {
     override init(imageName: String, bulletSound:String?)
     {
         super.init(imageName: imageName, bulletSound: bulletSound)
         
+        self.physicsBody = SKPhysicsBody(texture: self.texture!, size: self.size)
+        self.physicsBody?.isDynamic = true
+        self.physicsBody?.usesPreciseCollisionDetection = true
+        self.physicsBody?.categoryBitMask = CollisionCategories.InvaderBullet
+        self.physicsBody?.contactTestBitMask = CollisionCategories.Player
+        self.physicsBody?.collisionBitMask = 0x0
     }
     
     required public init?(coder aDecoder: NSCoder)
     {
         super.init(coder: aDecoder)
     }
-    
-    public func fireBullet(scene: SKScene) -> Void
-    {
-        
-    }
-    
+
 }
