7a1b360baf86aa773f2ab8408e983fce2b4a1982
willwpearson <31518711+willwpearson@users.noreply.github.com>
2017-12-19 12:40:20 -0700

    Added the collision methods for the player lasers.

--- William_CSP/Model/PlayerLaser.swift
+++ William_CSP/Model/PlayerLaser.swift
@@ -14,7 +14,12 @@ class PlayerLaser: Projectile
     override init(imageName: String, bulletSound:String?)
     {
         super.init(imageName: imageName, bulletSound: bulletSound)
-        
+        self.physicsBody = SKPhysicsBody(texture: self.texture!, size: self.size)
+        self.physicsBody?.isDynamic = true
+        self.physicsBody?.usesPreciseCollisionDetection = true
+        self.physicsBody?.categoryBitMask = CollisionCategories.PlayerBullet
+        self.physicsBody?.contactTestBitMask = CollisionCategories.Invader
+        self.physicsBody?.collisionBitMask = 0x0
     }
     
     required public init?(coder aDecoder: NSCoder)
