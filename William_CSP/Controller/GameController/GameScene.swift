a8bee508817dc93aaa30c1315a6c447cc8673f0f
willwpearson <31518711+willwpearson@users.noreply.github.com>
2018-01-03 12:49:55 -0700

    Added the method to allow the collisions to affect the player and invaders.

--- William_CSP/Controller/GameController/GameScene.swift
+++ William_CSP/Controller/GameController/GameScene.swift
@@ -73,7 +73,7 @@ public class GameScene: SKScene, SKPhysicsContactDelegate
             let invader = node as! SKSpriteNode
             let invaderHalfWidth = invader.size.width / 2
             invader.position.x -= CGFloat(self.invaderSpeed)
-            if(invader.position.x > self.rightBounds - invaderHalfWidth || invader.position.x < self.leftBounds + invaderHalfWidth)
+            if(invader.position.x > self.rightBounds + invaderHalfWidth || invader.position.x < self.leftBounds - invaderHalfWidth)
             {
                 changeDirection = true
             }
@@ -155,6 +155,7 @@ public class GameScene: SKScene, SKPhysicsContactDelegate
         
         backgroundColor = UIColor.black
         let starField = SKEmitterNode(fileNamed: "StarField")
+        starField?.zPosition = -1000
         addChild(starField!)
         rightBounds = self.size.width - 30
         setupInvaders()
@@ -225,6 +226,49 @@ public class GameScene: SKScene, SKPhysicsContactDelegate
         if((firstBody.categoryBitMask & CollisionCategories.Invader != 0) && (secondBody.categoryBitMask & CollisionCategories.Player != 0))
         {
             print("Invader and Player Collision Contact")
+        }
+        
+        if((firstBody.categoryBitMask & CollisionCategories.Player != 0) && (secondBody.categoryBitMask & CollisionCategories.InvaderBullet != 0))
+        {
+            player.die()
+        }
+        
+        if((firstBody.categoryBitMask & CollisionCategories.Invader != 0) && (secondBody.categoryBitMask & CollisionCategories.Player != 0))
+        {
+            player.kill()
+        }
+        
+        if((firstBody.categoryBitMask & CollisionCategories.Invader != 0) && (secondBody.categoryBitMask & CollisionCategories.PlayerBullet != 0))
+        {
+            if (contact.bodyA.node?.parent == nil || contact.bodyB.node?.parent == nil)
+            {
+                return
+            }
+            
+            let theInvader = firstBody.node as! Invader
+            let newInvaderRow = theInvader.invaderRow - 1
+            let newInvaderCol = theInvader.invaderCol
+            if(newInvaderRow >= 1)
+            {
+                self.enumerateChildNodes(withName: "invader")
+                {
+                    node, stop in
+                    let invader = node as! Invader
+                    if invader.invaderRow == newInvaderRow && invader.invaderCol == newInvaderCol
+                    {
+                        self.invadersThatCanFire.append(invader)
+                        stop.pointee = true
+                    }
+                }
+            }
+            let invaderIndex = invadersThatCanFire.index(of: firstBody.node as! Invader)
+            if(invaderIndex != nil)
+            {
+                invadersThatCanFire.remove(at: invaderIndex!)
+            }
+            theInvader.removeFromParent()
+            secondBody.node?.removeFromParent()
+            
         }
     }
     
