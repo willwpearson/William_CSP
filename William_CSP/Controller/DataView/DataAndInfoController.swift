163993fc0453ebf72a01b19ba9bd7828ea3f622d
willwpearson <31518711+willwpearson@users.noreply.github.com>
2018-01-09 12:14:12 -0700

    Added the necessary variables and methods for the Data view controller.

--- William_CSP/Controller/DataView/DataAndInfoController.swift
+++ William_CSP/Controller/DataView/DataAndInfoController.swift
@@ -11,5 +11,52 @@ import Foundation
 
 public class DataAndInfoController : UITableViewController
 {
-
+    lazy var bucketList : [BucketItem] =
+    {
+            return loadBucketListFromFile()
+    }()
+    
+    private func loadBucketListFromFile() -> [BucketItem]
+    {
+        var items = [BucketItem]()
+        if let filePath = Bundle.main.url(forResource: "bucket", withExtension: "csv")
+        {
+            do
+            {
+                let input = try String(contentsOf: filePath)
+                let bucketLines = input.components(separatedBy: "\n")
+                for line in bucketLines
+                {
+                    let item = line.components(separatedBy: ",")
+                    items.append(BucketItem(contents: item[0],author: item[1]))
+                }
+            }
+            catch
+            {
+                print("File load error")
+            }
+        }
+        
+        return items
+    }
+    
+    override public func numberOfSections(in tableView: UITableView) -> Int
+    {
+        return 1
+    }
+    
+    override public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int
+    {
+        return bucketList.count
+    }
+    
+    override public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell
+    {
+        let currentCell = tableView.dequeueReusableCell(withIdentifier: "dataIdentifier", for: indexPath) as! BucketItemCell
+        
+        currentCell.bucketItem = bucketList[indexPath.row]
+        //          currentCell.bucketItemSignature.text = currentCell.bucketItem.itemAuthor
+        
+        return currentCell
+    }
 }
